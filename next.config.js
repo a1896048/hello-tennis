2fe6c374d400be5e832901f991db7d94015e5525
lingzen <zengl5335@gmail.com>
2025-04-02 11:48:22 +1030

    Update deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['tpggbvxrgikvscazgage.supabase.co'],
+  },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: 'https://tpggbvxrgikvscazgage.supabase.co',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwZ2didnhyZ2lrdnNjYXpnYWdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ5NDE2MTYsImV4cCI6MjAyMDUxNzYxNn0.MxrmfYnhk9VHb_UMTNBrnUFJJ625IkfhX6yfsT',
+  },
+}
+
+module.exports = nextConfig
